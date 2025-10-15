88491b4a67882f45aed06aabc1793fd379a8c657
Tina Huynh <tinamchuynh@gmail.com>
2025-10-15 15:11:14 -0700

    Adds web scraping function for titles and links
    Implements a new function to scrape web page content, extracting titles, links, and artist names from specified HTML elements.
    
    Enhances data extraction capabilities for future use in data analysis or visualization tasks.

--- scripts/02-scrape-page-function.R
+++ scripts/02-scrape-page-function.R
@@ -4,3 +4,17 @@ library(tidyverse)
 library(rvest)
 library(tibble)
 library(stringr)
+
+# function: scrape_page --------------------------------------------------------
+
+#' Scrape web page content for titles, links, and artists
+scrape_page <- function(url) {
+  # read page
+  page <- read_html(url)
+
+  # scrape titles
+  titles <- page %>%
+    html_nodes(".iteminfo") %>%
+    html_node("h3 a") %>%
+    html_text() %>%
+    str_squish()
